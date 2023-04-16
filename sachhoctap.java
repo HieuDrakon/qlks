9158a55c3be01d8a8e3ac61a0e39e2b7b49efc48
Hiếu <124875874+HieuDrakon@users.noreply.github.com>
2023-04-16 17:21:16 +0700

    Update sachhoctap.java

--- sachhoctap.java
+++ sachhoctap.java
@@ -3,8 +3,8 @@ public class sachhoctap extends sach{
 
 public sachhoctap() {
 }
-public sachhoctap(String maSach,String tensach, String nhaXuatBan, double donGia, int soLuong) {
-super( maSach, tensach, nhaXuatBan,donGia,soLuong);
+public sachhoctap(String maSach, String tensach, String nhaXuatBan, double donGia, int soLuong,String ngaynhap,String ngayxuat) {
+super(maSach, tensach, nhaXuatBan,donGia,soLuong,ngaynhap, ngayxuat);
 }
 @Override
 public void hien()
