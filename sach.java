7b9dce786c8c09ec8e3d5688f2d96a80306ed0a6
Hiếu <124875874+HieuDrakon@users.noreply.github.com>
2023-04-17 22:27:47 +0700

    Update sach.java

--- sach.java
+++ sach.java
@@ -7,22 +7,25 @@ public class sach {
     String nhaXuatBan;
     double donGia;
     int soLuong;
-    String ngaynhap;
-    String ngayxuat; 
+    int ngn,tn,nn,ngx,tx,nx; 
     public sach() {
         super();
     }
  
-    public sach(String maSach,String tensach, String nhaXuatBan, double donGia, int soLuong,String ngaynhap,String ngayxuat ) {
+    public sach(String maSach,String tensach, String nhaXuatBan, double donGia, int soLuong,int ngn,int tn,int nn,int ngx,int tx,int nx ) {
         super();
         this.maSach = maSach;
         this.tensach=tensach;
         this.nhaXuatBan = nhaXuatBan;
         this.donGia = donGia;
         this.soLuong = soLuong;
-        this.ngaynhap = ngaynhap;
-        this.ngayxuat= ngayxuat;
-        
+        this.ngn = ngn;
+        this.tn= tn;
+        this.nn=nn;
+        this.ngx=ngx;
+        this.tx=tx;
+        this.nx=nx;
+
     }
  
     public String getMaSach() {
@@ -62,19 +65,45 @@ public class sach {
     public void setSoLuong(int soLuong) {
         this.soLuong = soLuong;
     }
-    public String getNgaynhap() {
-        return this.ngaynhap;
+    public int getNgn() {
+        return this.ngn;
     }
  
-    public void setNgaynhap(String ngaynhap) {
-        this.ngaynhap = ngaynhap;
+    public void setNgn(int ngn) {
+        this.ngn=ngn;
     }
-    public String getNgayxuat() {
-        return this.ngayxuat;
+    public int getTn() {
+        return this.tn;
     }
  
-    public void setNgayxuat(String ngayxuat) {
-        this.ngayxuat= ngayxuat;
+    public void settn(int tn) {
+        this.tn=tn;
+    }
+    public int getNn(){
+        return nn;
+    }
+    public void setNn(int nn){
+    this.nn=nn;
+    }
+    public int getNgx() {
+        return this.ngx;
+    }
+ 
+    public void setNgx(int ngx) {
+        this.ngx=ngx;
+    }
+    public int getTx() {
+        return this.tx;
+    }
+ 
+    public void setTx(int tx) {
+        this.tx=tx;
+    }
+    public int getNx(){
+        return nx;
+    }
+    public void setNx(int nx){
+    this.nx=nx;
     }
     public void nhap() {
         Scanner scanner = new Scanner(System.in);
@@ -84,9 +113,74 @@ public class sach {
         tensach = scanner.nextLine();
         System.out.println("Nhập tên nhà xuất bản: ");
         nhaXuatBan = scanner.nextLine();
+        System.out.println("Nhập tháng nhập");
+        tn = scanner.nextInt();
+        if(tn <1 && tn > 12){
+        do{
+         System.out.println("nhập sai nhập lại");
+         tn = scanner.nextInt();
+          }while(tn>= 1 && tn <=12)
+         } 
         System.out.println("Nhập ngày nhập");
-        ngaynhap = scanner.nextLine();
+        ngn = scanner.nextInt();
+        while(tn%2==0){
+        if(ngn<=0 && ngn > 31){
+        do{
+         System.out.println("nhập sai nhập lại");
+         ngn = scanner.nextInt();
+          }while(ngn>=1 && ngn <=31)
+        }
+        }
+        while(tn%2!=0){
+        if(ngn<=0 && ngn > 30){
+        do{
+         System.out.println("nhập sai nhập lại");
+         ngn = scanner.nextInt();
+          }while(ngn>=1 && ngn <=30)
+        }
+        }
+        System.out.println("Nhập năm nhập");
+        nn = scanner.nextInt();
+        if(nn < 2023 && nn > 2123){
+        do{
+         System.out.println("nhập sai nhập lại");
+         nn = scanner.nextInt();
+          }while(nn>=2023 && nn <=2123)
+        }
+        System.out.println("Nhập tháng xuất");
+        tx = scanner.nextInt();
+        if(tx <1 && tx > 12){
+        do{
+         System.out.println("nhập sai nhập lại");
+         tx = scanner.nextInt();
+          }while(tx>= 1 && tx <=12)
+         } 
         System.out.println("Nhập ngày xuất");
+        ngx = scanner.nextInt();
+        while(tx%2==0){
+        if(ngx<=0 && ngx > 31){
+        do{
+         System.out.println("nhập sai nhập lại");
+         ngx = scanner.nextInt();
+          }while(ngn>=1 && ngn <=31)
+        }
+        }
+        while(tn%2!=0){
+        if(ngn<=0 && ngn > 30){
+        do{
+         System.out.println("nhập sai nhập lại");
+         ngx = scanner.nextInt();
+          }while(ngx>=1 && ngx <=30)
+        }
+        }
+        System.out.println("Nhập năm xuất");
+        nx = scanner.nextInt();
+        if(nx < 2023 && nx > 2123){
+        do{
+         System.out.println("nhập sai nhập lại");
+         nx = scanner.nextInt();
+          }while(nx>=2023 && nx <=2123)
+         }
         ngayxuat = scanner.nextLine();     
         System.out.println("Nhập đơn giá: ");
         donGia = scanner.nextDouble();
@@ -97,6 +191,8 @@ public class sach {
     
     public void hien()
 {
+System.out.println(" thời gian nhập"+getNn()"/"+getTn()"/"+getNn());
 System.out.print("{mã sách: "+ getMaSach() + ", tên sách: "+getTensach() +", nhà xb: "+ getNhaXuatBan()+ ", đơn giá: "+getDonGia() + "00"+"số lượng: "+getSoLuong()+"ngày nhập"+getNgaynhap()+"ngày xuất"+getNgayxuat()+"}");
+System.out.println(" thời gian xuất"+getNx()+"/"+getTx()+"/"+getNx());
 }
 }
