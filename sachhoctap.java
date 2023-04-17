b4c21abf524cff4c4e8975381490c79b97113a48
Hiếu <124875874+HieuDrakon@users.noreply.github.com>
2023-04-17 22:33:31 +0700

    Update sachhoctap.java

--- sachhoctap.java
+++ sachhoctap.java
@@ -3,12 +3,15 @@ public class sachhoctap extends sach{
 
 public sachhoctap() {
 }
-public sachhoctap(String maSach, String tensach, String nhaXuatBan, double donGia, int soLuong,String ngaynhap,String ngayxuat) {
-super(maSach, tensach, nhaXuatBan,donGia,soLuong,ngaynhap, ngayxuat);
+public sachhoctap(String maSach, String tensach, String nhaXuatBan, double donGia, int soLuong,int ngn,int tn,int nn,int ngx, int tx, int nx) {
+super(maSach, tensach, nhaXuatBan,donGia,soLuong,ngn,tn,nn,ngx,tx,nx);
 }
 @Override
 public void hien()
 {
-super.hien();
+System.out.println(" thời gian nhập"+getNn()"/"+getTn()"/"+getNn());
+System.out.print("{mã sách: "+ getMaSach() + ", tên sách: "+getTensach() +", nhà xb: "+ getNhaXuatBan()+ ", đơn giá: "+getDonGia() + "00"+"số lượng: "+getSoLuong()+"ngày nhập"+getNgaynhap()+"ngày xuất"+getNgayxuat()+"}");
+System.out.println(" thời gian xuất"+getNx()+"/"+getTx()+"/"+getNx());
 }
 }
+
